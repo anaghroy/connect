48ee646e264e09c20c057afcc0e2d72f3a10c932
AnaghRoy <royanagh1999@gmail.com>
2026-01-08 16:28:34 +0530

    Minor Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
 import footerlogo from "../assets/images/footerlogo.png";
+import { useTheme } from "../context/ThemeContext";
 
 const Footer = () => {
+  const { theme } = useTheme();
   return (
     <div className="main-footer">
       <div className="outer">
@@ -9,10 +11,12 @@ const Footer = () => {
           {/** LOGO */}
           <div className="svg-logo">
             <img src={footerlogo} alt="footerlogo" />
-            <h1>Connect</h1>
+            <h1 style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
+              Connect
+            </h1>
           </div>
           <div className="text">
-            <p>
+            <p style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
               A freelance service web site connects businesses with freelancers,
               making project collaboration more efficient.
             </p>
@@ -25,33 +29,106 @@ const Footer = () => {
           </div>
         </div>
         <div className="product">
-          <p>Product</p>
-          <a href="#">About</a>
-          <a href="#">Team</a>
-          <a href="#">Careers</a>
+          <p style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
+            Product
+          </p>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            About
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Team
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Careers
+          </a>
         </div>
 
         <div className="product">
-          <p>Support</p>
-          <a href="#">How It Works</a>
-          <a href="#">Trust & Safety</a>
-          <a href="#">Help Centre</a>
+          <p style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
+            Support
+          </p>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            How It Works
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Trust & Safety
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Help Centre
+          </a>
         </div>
         <div className="product">
-          <p>Resources</p>
-          <a href="#">Customer Stories</a>
-          <a href="#">Cost Calculator</a>
-          <a href="#">Startup Cities</a>
+          <p style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
+            Resources
+          </p>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Customer Stories
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Cost Calculator
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Startup Cities
+          </a>
         </div>
       </div>
-      <div className="underline"></div>
+      <div className="underline" style={{color: theme === "light" ? "#ffffff" : "#f6fbfa"}}></div>
       <div className="copy">
-        <span>&copy; 2025 Connect Pvt. Ltd.</span>
+        <span style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}>
+          &copy; 2025 Connect Pvt. Ltd.
+        </span>
         <div className="links">
-          <a href="#">Terms</a>
-          <a href="#">Privacy</a>
-          <a href="#">Sitemap</a>
-          <a href="#">Company</a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Terms
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Privacy
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Sitemap
+          </a>
+          <a
+            style={{ color: theme === "light" ? "#ffffff" : "#f6fbfa" }}
+            href="#"
+          >
+            Company
+          </a>
         </div>
       </div>
     </div>
